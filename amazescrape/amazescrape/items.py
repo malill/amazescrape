710e15aef0042dac2aa31ae0aacc7eb4b70eec24
Markus Lill <markus.lill@gmx.de>
2023-11-17 10:43:49 +0100

    Retype AmazonItem

--- amazescrape/amazescrape/items.py
+++ amazescrape/amazescrape/items.py
@@ -28,25 +28,25 @@ class AmazonItem:
     url: str | None = field(default=None)
     request_timestamp: datetime | None = field(default=None)
 
+    # Basic info about the product
+    asin: str | None = field(default=None)
+    name: str | None = field(default=None)
+
     # Image
     image_urls: list[str] | None = field(default=None)
     images: None = field(default=None)
     image_filename: str | None = field(default=None)
 
-    # Basic info about the product
-    asin: str | None = field(default=None)
-    name: str | None = field(default=None)
-
     # Rating
-    rating_avg: str | None = field(default=None)
-    rating_n: str | None = field(default=None)
+    rating_avg: int | None = field(default=None)
+    rating_n: int | None = field(default=None)
 
     # Price
-    price: str | None = field(default=None)
-    price_strike: str | None = field(default=None)
+    price: int | None = field(default=None)
+    price_strike: int | None = field(default=None)
 
     # Page ranking
-    rank: str | None = field(default=None)
+    rank: int | None = field(default=None)
 
     # Badges
     status_badge_prop: str | None = field(default=None)
